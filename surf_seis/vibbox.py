20ed8de3a97313bbd950323ce53c2ef92d4c635f
cjhopp <chet.hopp@vuw.ac.nz>
2020-03-27 10:55:11 -0700

    clock signal debug

--- surf_seis/vibbox.py
+++ surf_seis/vibbox.py
@@ -211,6 +211,7 @@ def vibbox_read(fname, param):
         f.seek(HEADER_OFFSET, os.SEEK_SET)
         # read header
         H = np.fromfile(f, dtype=np.int32, count=HEADER_SIZE)
+        print(H)
         BUFFER_SIZE=H[0]
         FREQUENCY=H[1]
         NUM_OF_BUFFERS=H[2]
